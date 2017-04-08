1e25cb18bd79d3afcd022df75667f0a16bf810f6
Chathuranga Sineth <mcsineth@gmail.com>
2017-04-08 22:16:20 +0530

    Add files via upload

---
+++ Ransomware/src/Ransomware_Control/App_Control.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ransomware_Control;
+
+/**
+ *
+ * @author S!N3TH
+ */
+public class App_Control {
+    
+}
